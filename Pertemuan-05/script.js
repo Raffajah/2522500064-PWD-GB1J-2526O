4ebce60af586360c9a95ff45724b586f1b3db6d2
raffajah <rafaalghifari205@gmail.com>
2025-10-25 13:26:05 +0700

    menambah pesan di java

--- Pertemuan-05/script.js
+++ Pertemuan-05/script.js
@@ -1,5 +1,10 @@
 document.getElementById("menutogle").addEventListener("click", function()
 {document.querySelector("nav").classList.toggle("active"); });
+alert ("Halo. Saya adalah Rafa Alghifari yang membuat web ini");
+alert("Siapa kamu?");
+let nama = prompt("Masukkan Nama");
+alert("Halo Selamat Datang " + nama + "!");
+console.log("Data Berhasil");
 
-let nama = prompt("Siapa nama kamu?");
-alert("Halo, " + nama + "!");
+
+
