8503718c71fd66e9d8c2e982c2ac19d2215743f3
raffajah <rafaalghifari205@gmail.com>
2025-11-02 22:12:48 +0700

    perhitungan otomatis

--- Pertemuan-06/README.md
+++ Pertemuan-06/README.md
@@ -15,4 +15,5 @@ Hari sabtu 01,11,2025. saya belajar <ol>
 <li> membuat variabel matkul 5 </li> 
 <li> mengroup section ipk dengan about dan styling tipis2 </li> 
 <li> membuat variabel perhitugan 1 sampai 5 </li> 
+<li> membuat perhitugan otomatis 1 sampai 5 </li> 
 <ol>
--- Pertemuan-06/index.php
+++ Pertemuan-06/index.php
@@ -92,6 +92,206 @@
         $nilaiAkhir4 = (0.1 * $nilaihadir4) + (0.2 * $nilaitugas4) + (0.3 * $nilaiuts4) + (0.4 * $nilaiuas4);
         $nilaiAkhir5 = (0.1 * $nilaihadir5) + (0.2 * $nilaitugas5) + (0.3 * $nilaiuts5) + (0.4 * $nilaiuas5);
 
+        if ($nilaihadir1 < 70){ 
+            $grade1="E";
+            $mutu1="0.00";
+            $status1="Gagal";
+        } else {
+            if ($nilaiAkhir1>=91){
+                $grade1="A";
+                $mutu1=4.00;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=81)
+                $grade1="A-";
+                $mutu1=3.70;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=76)
+                $grade1="B+";
+                $mutu1=3.30;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=71)
+                $grade1="B";
+                $mutu1=3.00;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=66)
+                $grade1="B-";
+                $mutu1=2.70;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=61)
+                $grade1="C+";
+                $mutu1=2.30;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=56)
+                $grade1="C";
+                $mutu1=2.00;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=51)
+                $grade1="C-";
+                $mutu1=1.70;
+                $status1="Lulus";
+            } elseif {
+                ($nilaiAkhir1>=36)
+                $grade1="D";
+                $mutu1=1.00;
+                $status1="Gagal";
+            } else {
+                ($nilaiAkhir1>=0)
+                $grade1="E";
+                $mutu1=0.00;
+                $status1="Gagal";
+            }
+        }
+
+         if ($nilaihadir2 < 70){ 
+            $grade2="E";
+            $mutu2="0.00";
+            $status2="Gagal";
+        } else {
+            if ($nilaiAkhir2>=91){
+                $grade2="A";
+                $mutu2=4.00;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=81)
+                $grade2="A-";
+                $mutu2=3.70;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=76)
+                $grade2="B+";
+                $mutu2=3.30;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=71)
+                $grade2="B";
+                $mutu2=3.00;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=66)
+                $grade2="B-";
+                $mutu2=2.70;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=61)
+                $grade2="C+";
+                $mutu2=2.30;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=56)
+                $grade2="C";
+                $mutu2=2.00;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=51)
+                $grade2="C-";
+                $mutu2=1.70;
+                $status2="Lulus";
+            } elseif {
+                ($nilaiAkhir2>=36)
+                $grade2="D";
+                $mutu2=1.00;
+                $status2="Gagal";
+            } else {
+                ($nilaiAkhir2>=0)
+                $grade2="E";
+                $mutu2=0.00;
+                $status2="Gagal";
+            }
+        }
+
+                if ($nilaihadir3 < 70){ 
+            $grade3 = "E";
+            $mutu3 = 0.00;
+            $status3 = "Gagal";
+        } else {
+            if ($nilaiAkhir3 >= 91) {
+                $grade3 = "A"; $mutu3 = 4.00; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 81) {
+                $grade3 = "A-"; $mutu3 = 3.70; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 76) {
+                $grade3 = "B+"; $mutu3 = 3.30; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 71) {
+                $grade3 = "B"; $mutu3 = 3.00; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 66) {
+                $grade3 = "B-"; $mutu3 = 2.70; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 61) {
+                $grade3 = "C+"; $mutu3 = 2.30; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 56) {
+                $grade3 = "C"; $mutu3 = 2.00; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 51) {
+                $grade3 = "C-"; $mutu3 = 1.70; $status3 = "Lulus";
+            } elseif ($nilaiAkhir3 >= 36) {
+                $grade3 = "D"; $mutu3 = 1.00; $status3 = "Gagal";
+            } else {
+                $grade3 = "E"; $mutu3 = 0.00; $status3 = "Gagal";
+            }
+        }
+
+                if ($nilaihadir4 < 70){ 
+            $grade4 = "E";
+            $mutu4 = 0.00;
+            $status4 = "Gagal";
+        } else {
+            if ($nilaiAkhir4 >= 91) {
+                $grade4 = "A"; $mutu4 = 4.00; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 81) {
+                $grade4 = "A-"; $mutu4 = 3.70; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 76) {
+                $grade4 = "B+"; $mutu4 = 3.30; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 71) {
+                $grade4 = "B"; $mutu4 = 3.00; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 66) {
+                $grade4 = "B-"; $mutu4 = 2.70; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 61) {
+                $grade4 = "C+"; $mutu4 = 2.30; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 56) {
+                $grade4 = "C"; $mutu4 = 2.00; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 51) {
+                $grade4 = "C-"; $mutu4 = 1.70; $status4 = "Lulus";
+            } elseif ($nilaiAkhir4 >= 36) {
+                $grade4 = "D"; $mutu4 = 1.00; $status4 = "Gagal";
+            } else {
+                $grade4 = "E"; $mutu4 = 0.00; $status4 = "Gagal";
+            }
+        }
+
+                if ($nilaihadir5 < 70){ 
+            $grade5 = "E";
+            $mutu5 = 0.00;
+            $status5 = "Gagal";
+        } else {
+            if ($nilaiAkhir5 >= 91) {
+                $grade5 = "A"; $mutu5 = 4.00; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 81) {
+                $grade5 = "A-"; $mutu5 = 3.70; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 76) {
+                $grade5 = "B+"; $mutu5 = 3.30; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 71) {
+                $grade5 = "B"; $mutu5 = 3.00; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 66) {
+                $grade5 = "B-"; $mutu5 = 2.70; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 61) {
+                $grade5 = "C+"; $mutu5 = 2.30; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 56) {
+                $grade5 = "C"; $mutu5 = 2.00; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 51) {
+                $grade5 = "C-"; $mutu5 = 1.70; $status5 = "Lulus";
+            } elseif ($nilaiAkhir5 >= 36) {
+                $grade5 = "D"; $mutu5 = 1.00; $status5 = "Gagal";
+            } else {
+                $grade5 = "E"; $mutu5 = 0.00; $status5 = "Gagal";
+            }
+        }
+
+
+
         ?>
 
         <h2>Nilai Saya</h2>
