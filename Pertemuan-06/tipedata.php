ac73ed8a3e67fa4373c86577e06b6f1dc88c6301
raffajah <rafaalghifari205@gmail.com>
2025-11-02 12:27:05 +0700

    mencoba latihan tipe data

---
+++ Pertemuan-06/tipedata.php
@@ -0,0 +1,14 @@
+<?php
+$nama = "Rafa Alghifari";
+$umur = 19;
+$tinggi = 175;
+$aktif = true;
+echo "Nama: $nama <br>";
+echo "Umur: $umur tahun <br>";
+echo "Tinggi: $tinggi meter <br>";
+echo "Status aktif: " . ($aktif ? "Ya" : "Tidak") . "<br>";
+var_dump($nama);
+var_dump($umur);
+var_dump($tinggi);
+var_dump($aktif);
+?>
