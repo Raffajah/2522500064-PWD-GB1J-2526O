a2cdd94e3d270a6900ba4ffb890bdc643e14c604
Raffajah <Rafaalghifari205@gmail.com>
2025-11-23 13:50:44 +0700

    menambah folder uji

--- Pertemuan-09/halo.py
+++
@@ -1,5 +0,0 @@
-uangtabungan = 100
-uangbank = 500
-total = (uangtabungan+uangbank)
-
-print ("halo nama saya rafa alghifari dan saya mencoba membuat file py!, Oh ya dan saya juga punya uang" , total)
