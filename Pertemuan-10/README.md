5023befebf9641f754a4435e64f9b0c83f4c245a
Raffajah <Rafaalghifari205@gmail.com>
2025-12-04 16:34:52 +0700

    db_pwd2025 sql

--- Pertemuan-10/README.md
+++ Pertemuan-10/README.md
@@ -14,4 +14,5 @@ kamis, 04 Desember 2025<br>
 <li>form comtact read inc</li>
 <li>include read inc index.php</li>
 <li>beres</li>
+<li>menambah file db_pwd2025 sql</li>
 </ol>
---
+++ Pertemuan-10/db_pwd2025.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 04 Des 2025 pada 09.31
+-- Versi server: 5.7.33
+-- Versi PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int(11) NOT NULL,
+  `cnama` varchar(100) DEFAULT NULL,
+  `cemail` varchar(100) DEFAULT NULL,
+  `cpesan` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`) VALUES
+(1, 'Rafa Alghifari', 'Rafaalghifari@gmail.com', 'Hai.. Aku Rafa dan aku ingin sekali ingin pergi ke jepang'),
+(2, 'Hatsune Miku', 'mik@gmail.com', 'beppppp'),
+(3, 'Kasane Teto', 'Tettooo@gmail.com', 'Beppppppppppp toooo');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
