e13b4869f3dd118537ffe586b37c16675c2b52fe
Raffajah <rafaalghifari205@gmail.com>
2025-12-15 14:05:33 +0700

    upgrade

--- Pertemuan-11/read.php
+++ Pertemuan-11/read.php
@@ -25,7 +25,7 @@ $no = 1;
         <td><?= htmlspecialchars($row['cnama']); ?></td>
         <td><?= htmlspecialchars($row['cemail']); ?></td>
         <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-        <td><?= htmlspecialchars($row['created_at']); ?></td>
+        <td><?= htmlspecialchars($row['dcreated_at']); ?></td>
             
     </tr>
     <?php endwhile; ?>
