272a0f65eaf8b695482358a4623fabb7ed356760
Raffajah <rafaalghifari205@gmail.com>
2026-01-08 15:27:12 +0700

    link delete

--- Pertemuan-13/README.md
+++ Pertemuan-13/README.md
@@ -2,4 +2,5 @@ rafa alghifari <br>
 2522500064<br>
 <ol>
 <li>copy item</li>
+<li>menambah link delete dari read.php</li>
 </ol>
--- Pertemuan-13/read.php
+++ Pertemuan-13/read.php
@@ -48,7 +48,7 @@ unset($_SESSION['sinar_sukses'], $_SESSION['sinar_error']);
         <td><?= $no++; ?></td>
         <td>
           <a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
-          <a href="delete.php?cid=<?= (int)$row['cid']; ?>">Delete</a>
+          <a href="proses_delete.php">Delete</a>
         </td>
         <td><?= $row['cid']; ?></td>
         <td><?= htmlspecialchars($row['cnama']); ?></td>
